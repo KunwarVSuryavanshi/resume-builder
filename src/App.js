3a7fdeb8a524d3876051f4a0d0cf302cab00d6a8
KunwarVSuryavanshi <virajkunwar98@gmail.com>
2022-07-04 17:13:11 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import ReactToPDF from '@kunwarji/react-to-pdf'
+import { useRef } from 'react'
 
 function App() {
+  const refVar = useRef()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ReactToPDF element={refVar} scale={1}>
+        {(toPdf) => (
+          <button type="button" onClick={toPdf}>
+            Click me
+          </button>
+        )
+        }
+      </ReactToPDF>
+      <div ref={refVar}>
+        Something inside APP.js
+      </div>
     </div>
   );
 }
